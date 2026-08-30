01cb8ea61fc52f897a006197cbf392f2a8e453cc
fazlanfazrie-rgb <fazlanfazrie@gmail.com>
2026-08-31 00:07:59 +0800

    fix: use package imports in report tests

--- test/report_engine_test.dart
+++ test/report_engine_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import '../lib/core/report_engine.dart';
+import 'package:storeph3/core/report_engine.dart';
 
 void main() {
   final rows = <Map<String, Object?>>[
