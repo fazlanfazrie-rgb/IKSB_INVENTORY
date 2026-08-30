566aa8fa4d23b9870c0224e159ef570eebc0e649
fazlanfazrie-rgb <fazlanfazrie@gmail.com>
2026-08-31 00:07:13 +0800

    fix: use package imports in fertilizer tests

--- test/fertilizer_engine_test.dart
+++ test/fertilizer_engine_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import '../lib/core/fertilizer_engine.dart';
+import 'package:storeph3/core/fertilizer_engine.dart';
 
 void main() {
   final items = [
